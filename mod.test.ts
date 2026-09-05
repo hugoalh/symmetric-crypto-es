3931f20e75053d0b62d49c6196fabd28876a1d85
hugoalh <hugoalh@gmail.com>
2026-09-05 18:37:19 +0800

    Refactor 5 part 1

--- mod.test.ts
+++ mod.test.ts
@@ -1,16 +1,51 @@
 import {
-	decodeAscii85,
-	encodeAscii85
-} from "jsr:@std/encoding@^1.0.10/ascii85";
-import { deepStrictEqual } from "node:assert";
-import { SymmetricCryptor } from "./mod.ts";
-const ignore = !(
-	Deno.args.includes("--force") ||
-	Deno.env.get("GITHUB_ACTIONS") === "true"
-);
-const sample1String = "qwertyuiop";
-const sample1UInt8 = new TextEncoder().encode(sample1String);
-const sample2String = `Accusam lorem nisl amet feugait commodo liber et. Diam sed amet et kasd et id lorem accusam voluptua elitr eirmod et justo diam clita consequat consetetur. Odio nonumy sadipscing dolor minim voluptua gubergren dolore vulputate vero dolor at sed lorem vero stet. Accusam justo ut lorem invidunt justo invidunt lobortis nobis. Erat duo ipsum sit eirmod lorem stet dolore dolor ipsum. Ipsum consetetur sit elitr et sit eum amet dolor et ut sanctus praesent sed et sed et.
+	deepStrictEqual,
+	doesNotReject,
+	doesNotThrow
+} from "node:assert";
+import {
+	createSymmetricCrypto,
+	createSymmetricCryptoSync,
+	type SymmetricCryptoAlgorithm
+} from "./mod.ts";
+const algorithms: readonly SymmetricCryptoAlgorithm[] = [/* UNIQUE */
+	"aes-128-cbc",
+	"aes-128-ctr",
+	"aes-128-ecb",
+	// "aes-128-gcm",
+	"aes-192-ctr",
+	"aes-192-ecb",
+	"aes-256-cbc",
+	"aes-256-ctr",
+	"aes-256-ecb",
+	// "aes-256-gcm"
+];
+Deno.test("Key Empty Async", { permissions: "none" }, async () => {
+	await doesNotReject(async () => {
+		await createSymmetricCrypto("");
+	});
+});
+Deno.test("Key Empty Sync", { permissions: "none" }, () => {
+	doesNotThrow(() => {
+		createSymmetricCryptoSync("");
+	});
+});
+async function testerRaw(t: Deno.TestContext, key: string, context: string): Promise<void> {
+	const sample = new TextEncoder().encode(context);
+	for (const algorithm of algorithms) {
+		await t.step(algorithm, async () => {
+			const cryptor = await createSymmetricCrypto(key, { algorithm });
+			const encrypted = cryptor.encrypt(sample);
+			console.log(encrypted);
+			deepStrictEqual(cryptor.decrypt(encrypted), sample);
+		});
+	}
+}
+Deno.test("Raw 1", { permissions: "none" }, async (t) => {
+	await testerRaw(t, "QwErTyUiOp", "qwertyuiop");
+});
+Deno.test("Raw 2", { permissions: "none" }, async (t) => {
+	await testerRaw(t, "QwErTyUiOp", `Accusam lorem nisl amet feugait commodo liber et. Diam sed amet et kasd et id lorem accusam voluptua elitr eirmod et justo diam clita consequat consetetur. Odio nonumy sadipscing dolor minim voluptua gubergren dolore vulputate vero dolor at sed lorem vero stet. Accusam justo ut lorem invidunt justo invidunt lobortis nobis. Erat duo ipsum sit eirmod lorem stet dolore dolor ipsum. Ipsum consetetur sit elitr et sit eum amet dolor et ut sanctus praesent sed et sed et.
 
 Sanctus veniam rebum eleifend magna amet est sanctus no accusam rebum in nisl ea nulla takimata at nulla. Zzril et minim lorem aliquip sea amet clita consequat gubergren et voluptua dolor sed dolore sed consequat dolores stet. No labore sed molestie stet dolore diam amet diam ut. Sed ipsum gubergren velit eos duis takimata nulla invidunt justo accusam justo. Invidunt nulla iriure clita accumsan vero voluptua dolor. Gubergren tempor dolore minim sed sed aliquam consequat eleifend eirmod clita te eu. Feugiat justo dolore dolor eum takimata diam sit iusto delenit feugiat ipsum dolore exerci et nonumy et vel elitr. Eirmod in placerat consequat dolor ea est. Eirmod dolore facilisis invidunt eirmod. Kasd diam takimata imperdiet dolor illum elitr elitr autem vel. Augue sadipscing rebum sit amet eos aliquyam praesent tempor diam nonumy feugiat dolores kasd sed dolor. Consectetuer vulputate nonumy iriure gubergren et vel consetetur dolore esse magna diam dolore delenit. Sanctus stet eirmod. Eros vulputate elitr no.
 
@@ -18,182 +53,5 @@ Sit minim accusam elitr vulputate adipiscing vero consectetuer sea no no consequ
 
 Accusam dolores et eirmod erat sadipscing lorem illum erat commodo vero gubergren. Ipsum facilisis et elit nonumy amet clita nonumy duis eirmod lorem dolores aliquip in sed at. Vero eirmod duo laoreet magna duo consetetur et et takimata. Dolore dignissim erat dolore accumsan stet diam diam gubergren eirmod aliquyam accusam et accusam et nulla et stet. Illum ut quod dolor magna ut elitr elit ullamcorper duis. Erat diam sed hendrerit vero sed ut eos veniam sanctus magna. Ea lorem iriure enim ut suscipit possim labore et volutpat. Placerat qui nisl at ipsum dolor diam dolor accusam. Diam sadipscing diam gubergren vulputate dolor dolore eirmod lorem gubergren blandit duo aliquyam. Rebum consetetur invidunt takimata voluptua et no voluptua aliquyam vel. Vero ut dolores. Feugait erat et. Sanctus dolor takimata lorem et clita sea accusam labore iusto et. Esse eirmod sed facilisis kasd. Diam elitr eos diam.
 
-Takimata sea takimata est sit kasd et est lorem nibh in est diam. Ipsum vulputate erat amet invidunt justo te ipsum eos ipsum sed dolor. Amet no et diam. Amet ut et gubergren amet ut sed accusam duis et. Iriure kasd amet amet. In dolor sit hendrerit gubergren nulla et sea autem sanctus diam eos. Magna nonummy labore delenit clita lorem vero eirmod et nonumy sadipscing et ipsum elitr vel consetetur nonumy. Praesent eum at lobortis consequat dolor ut sanctus sadipscing sit. Accusam consetetur no velit aliquam et lorem assum in illum sed sea et et aliquip sea quod amet. Dolor zzril ut et sadipscing vero ut id dolore eu veniam velit kasd. Erat lorem sit consequat feugiat tation at sed dolore dolor sea autem in sadipscing dolore sed.`;
-const sample2UInt8 = new TextEncoder().encode(sample2String);
-Deno.test("Decrypt String 1 Base64", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!");
-	deepStrictEqual(await cryptor.decrypt("6zUMUyY3gQaKqCZZOcFGucdlpnQa5i97PfypJpByA+Y="), sample1String);
-});
-Deno.test("Decrypt String 1 Base64URL", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!", { cipherTextCoder: "base64url" });
-	deepStrictEqual(await cryptor.decrypt("6zUMUyY3gQaKqCZZOcFGucdlpnQa5i97PfypJpByA-Y="), sample1String);
-});
-Deno.test("Decrypt String 1 ASCII85", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!", {
-		cipherTextCoder: {
-			decoder(data) {
-				return decodeAscii85(data);
-			},
-			encoder(data) {
-				return encodeAscii85(data);
-			}
-		}
-	});
-	deepStrictEqual(await cryptor.decrypt("lST)L-9$J[MPqk)3Pe1qa(;,i)Wi]\"4oD9+OE(Hc"), sample1String);
-});
-Deno.test("Full String AES-CBC 1", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!");
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full String AES-CBC 100", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!", { times: 100 });
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full UInt8 AES-CBC 1", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!");
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full UInt8 AES-CBC 100", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor("<PassWord123456>!!", { times: 100 });
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full String AES-CTR 1", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-CTR",
-		key: "<PassWord123456>!!"
-	});
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full String AES-CTR 100", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-CTR",
-		key: "<PassWord123456>!!"
-	}, { times: 100 });
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full UInt8 AES-CTR 1", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-CTR",
-		key: "<PassWord123456>!!"
-	});
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full UInt8 AES-CTR 100", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-CTR",
-		key: "<PassWord123456>!!"
-	}, { times: 100 });
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full String AES-GCM 1", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-GCM",
-		key: "<PassWord123456>!!"
-	});
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full String AES-GCM 100", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-GCM",
-		key: "<PassWord123456>!!"
-	}, { times: 100 });
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full UInt8 AES-GCM 1", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-GCM",
-		key: "<PassWord123456>!!"
-	});
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full UInt8 AES-GCM 100", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor({
-		algorithm: "AES-GCM",
-		key: "<PassWord123456>!!"
-	}, { times: 100 });
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full String AES-CBC,AES-CTR,AES-GCM", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor([
-		{ algorithm: "AES-CBC", key: "<PassWord123456>!!" },
-		{ algorithm: "AES-CTR", key: "<PassWord123456>!!" },
-		{ algorithm: "AES-GCM", key: "<PassWord123456>!!" }
-	]);
-	const encrypted = await cryptor.encrypt(sample1String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1String);
-});
-Deno.test("Full UInt8 AES-CBC,AES-CTR,AES-GCM", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor([
-		{ algorithm: "AES-CBC", key: "<PassWord123456>!!" },
-		{ algorithm: "AES-CTR", key: "<PassWord123456>!!" },
-		{ algorithm: "AES-GCM", key: "<PassWord123456>!!" }
-	]);
-	const encrypted = await cryptor.encrypt(sample1UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample1UInt8);
-});
-Deno.test("Full String Large AES-CBC,AES-CTR,AES-GCM", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor([
-		{ algorithm: "AES-CBC", key: "<PassWord123456>!!" },
-		{ algorithm: "AES-CTR", key: "<PassWord987654>!!" },
-		{ algorithm: "AES-GCM", key: "<PassWord123456>!!" }
-	]);
-	const encrypted = await cryptor.encrypt(sample2String);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample2String);
-});
-Deno.test("Full UInt8 Large AES-CBC,AES-CTR,AES-GCM", { permissions: "none" }, async () => {
-	const cryptor = new SymmetricCryptor([
-		{ algorithm: "AES-CBC", key: "<PassWord123456>!!" },
-		{ algorithm: "AES-CTR", key: "<PassWord987654>!!" },
-		{ algorithm: "AES-GCM", key: "<PassWord123456>!!" }
-	]);
-	const encrypted = await cryptor.encrypt(sample2UInt8);
-	console.log(encrypted);
-	deepStrictEqual(await cryptor.decrypt(encrypted), sample2UInt8);
-});
-Deno.test("Full File Large AES-CBC,AES-CTR,AES-GCM", {
-	ignore,
-	permissions: {
-		read: true,
-		write: true
-	}
-}, async () => {
-	const tempfile = await Deno.makeTempFile();
-	try {
-		const cryptor = new SymmetricCryptor([
-			{ algorithm: "AES-CBC", key: "<PassWord123456>!!" },
-			{ algorithm: "AES-CTR", key: "<PassWord987654>!!" },
-			{ algorithm: "AES-GCM", key: "<PassWord123456>!!" }
-		]);
-		await cryptor.writeEncryptedTextFile(tempfile, sample2String);
-		deepStrictEqual(await cryptor.readEncryptedTextFile(tempfile), sample2String);
-	} finally {
-		await Deno.remove(tempfile);
-	}
+Takimata sea takimata est sit kasd et est lorem nibh in est diam. Ipsum vulputate erat amet invidunt justo te ipsum eos ipsum sed dolor. Amet no et diam. Amet ut et gubergren amet ut sed accusam duis et. Iriure kasd amet amet. In dolor sit hendrerit gubergren nulla et sea autem sanctus diam eos. Magna nonummy labore delenit clita lorem vero eirmod et nonumy sadipscing et ipsum elitr vel consetetur nonumy. Praesent eum at lobortis consequat dolor ut sanctus sadipscing sit. Accusam consetetur no velit aliquam et lorem assum in illum sed sea et et aliquip sea quod amet. Dolor zzril ut et sadipscing vero ut id dolore eu veniam velit kasd. Erat lorem sit consequat feugiat tation at sed dolore dolor sea autem in sadipscing dolore sed.`);
 });
--- mod.ts
+++ mod.ts
@@ -1,15 +1,196 @@
-export {
-	SymmetricCryptorBasic,
-	type SymmetricCryptorAlgorithm,
-	type SymmetricCryptorCipherTextCoderDefault,
-	type SymmetricCryptorCipherTextCoderOptions,
-	type SymmetricCryptorCipherTextDecoder,
-	type SymmetricCryptorCipherTextEncoder,
-	type SymmetricCryptorKeyInput,
-	type SymmetricCryptorKeyType,
-	type SymmetricCryptorOptions
-} from "./basic.ts";
-export {
-	SymmetricCryptor,
-	SymmetricCryptor as default
-} from "./standard.ts";
+import {
+	createCipheriv,
+	createDecipheriv,
+	createSecretKey,
+	getCipherInfo,
+	getCiphers,
+	randomBytes,
+	scrypt,
+	scryptSync,
+	type BinaryLike,
+	type CipherInfo,
+	type Cipheriv,
+	type Decipheriv,
+	type KeyObject,
+	type ScryptOptions
+} from "node:crypto";
+export type SymmetricCryptoAlgorithm =
+	| "aes-128-cbc"
+	| "aes-128-ctr"
+	| "aes-128-ecb"
+	/* | "aes-128-gcm" */
+	| "aes-192-ctr"
+	| "aes-192-ecb"
+	| "aes-256-cbc"
+	| "aes-256-ctr"
+	| "aes-256-ecb"
+	/* | "aes-256-gcm" */;
+const algorithmsSelect: readonly SymmetricCryptoAlgorithm[] = [/* UNIQUE */
+	"aes-128-cbc",
+	"aes-128-ctr",
+	"aes-128-ecb",
+	// "aes-128-gcm",
+	"aes-192-ctr",
+	"aes-192-ecb",
+	"aes-256-cbc",
+	"aes-256-ctr",
+	"aes-256-ecb",
+	// "aes-256-gcm"
+];
+const algorithmsSupport: readonly string[] = getCiphers();
+const algorithmsFinal: readonly string[] = algorithmsSupport.filter((algorithm: string): boolean => {
+	return algorithmsSelect.includes(algorithm as SymmetricCryptoAlgorithm);
+}).sort();
+class SymmetricCryptorDecryptPipe extends TransformStream<Uint8Array, Uint8Array> {
+	#algorithm: SymmetricCryptoAlgorithm;
+	#bin: Uint8Array = new Uint8Array();
+	#decipher?: Decipheriv;
+	#ivLength: number;
+	#key: KeyObject;
+	constructor(algorithm: SymmetricCryptoAlgorithm, ivLength: number, key: KeyObject) {
+		super({
+			transform: (chunk: Uint8Array, controller: TransformStreamDefaultController<Uint8Array>): void => {
+				if (this.#decipher) {
+					controller.enqueue(this.#decipher.update(chunk));
+				} else {
+					this.#bin = Uint8Array.from([...this.#bin, ...chunk]);
+					if (this.#bin.length >= this.#ivLength) {
+						const iv: Uint8Array = this.#bin.slice(0, this.#ivLength);
+						const context: Uint8Array = this.#bin.slice(this.#ivLength);
+						this.#decipher = createDecipheriv(this.#algorithm, this.#key, iv);
+						if (context.length > 0) {
+							controller.enqueue(this.#decipher.update(context));
+						}
+					}
+				}
+			},
+			flush: (controller: TransformStreamDefaultController<Uint8Array>): void => {
+				if (this.#decipher) {
+					controller.enqueue(this.#decipher.final());
+				}
+			}
+		});
+		this.#algorithm = algorithm;
+		this.#ivLength = ivLength;
+		this.#key = key;
+	}
+}
+class SymmetricCryptorEncryptPipe extends TransformStream<Uint8Array, Uint8Array> {
+	#cipher: Cipheriv;
+	#iv: Uint8Array;
+	constructor(algorithm: SymmetricCryptoAlgorithm, ivLength: number, key: KeyObject) {
+		super({
+			start: (controller: TransformStreamDefaultController<Uint8Array>): void => {
+				controller.enqueue(this.#iv);
+			},
+			transform: (chunk: Uint8Array, controller: TransformStreamDefaultController<Uint8Array>): void => {
+				controller.enqueue(Uint8Array.from(this.#cipher.update(chunk)));
+			},
+			flush: (controller: TransformStreamDefaultController<Uint8Array>): void => {
+				controller.enqueue(Uint8Array.from(this.#cipher.final()));
+			}
+		});
+		this.#iv = randomBytes(ivLength);
+		this.#cipher = createCipheriv(algorithm, key, this.#iv);
+	}
+}
+export interface SymmetricCryptorOptions {
+	/**
+	 * Algorithm of the cipher.
+	 * 
+	 * Availability of the cipher algorithms are depend on the runtime.
+	 * @default {"aes-256-cbc"}
+	 */
+	algorithm?: SymmetricCryptoAlgorithm;
+	/**
+	 * Options of the scrypt.
+	 */
+	scrypt?: ScryptOptions;
+}
+export interface SymmetricCryptorInterface {
+	createDecryptPipe(): TransformStream<Uint8Array>;
+	createEncryptPipe(): TransformStream<Uint8Array>;
+	decrypt(data: Uint8Array): Uint8Array;
+	encrypt(data: Uint8Array): Uint8Array;
+}
+class SymmetricCryptor implements SymmetricCryptorInterface {
+	get [Symbol.toStringTag](): string {
+		return "SymmetricCryptor";
+	}
+	#algorithm: SymmetricCryptoAlgorithm;
+	#ivLength: number;
+	#key: KeyObject;
+	constructor(algorithm: SymmetricCryptoAlgorithm, ivLength: number, key: KeyObject) {
+		this.#algorithm = algorithm;
+		this.#ivLength = ivLength;
+		this.#key = key;
+	}
+	createDecryptPipe(): TransformStream<Uint8Array> {
+		return new SymmetricCryptorDecryptPipe(this.#algorithm, this.#ivLength, this.#key);
+	}
+	createEncryptPipe(): TransformStream<Uint8Array> {
+		return new SymmetricCryptorEncryptPipe(this.#algorithm, this.#ivLength, this.#key);
+	}
+	decrypt(data: Uint8Array): Uint8Array {
+		const iv: Uint8Array = data.slice(0, this.#ivLength);
+		const context: Uint8Array = data.slice(this.#ivLength);
+		const decipher: Decipheriv = createDecipheriv(this.#algorithm, this.#key, iv);
+		const resultUpdate: Uint8Array = Uint8Array.from(decipher.update(context));
+		const resultFinal: Uint8Array = Uint8Array.from(decipher.final());
+		return Uint8Array.from([...resultUpdate, ...resultFinal]);
+	}
+	encrypt(data: Uint8Array): Uint8Array {
+		const iv: Uint8Array = randomBytes(this.#ivLength);
+		const cipher: Cipheriv = createCipheriv(this.#algorithm, this.#key, iv);
+		const resultUpdate: Uint8Array = Uint8Array.from(cipher.update(data));
+		const resultFinal: Uint8Array = Uint8Array.from(cipher.final());
+		return Uint8Array.from([...iv, ...resultUpdate, ...resultFinal]);
+	}
+}
+function resolveSymmetricCryptorInfo(algorithm: SymmetricCryptoAlgorithm): CipherInfo {
+	if (!algorithmsSelect.includes(algorithm.toLowerCase() as SymmetricCryptoAlgorithm)) {
+		throw new Error(`\`${algorithm}\` is not a valid cipher algorithm! Only accept these values: ${algorithmsSelect.join(", ")}`);
+	}
+	if (!algorithmsSupport.includes(algorithm.toLowerCase())) {
+		throw new Error(`\`${algorithm}\` is not a cipher algorithm which support by the runtime! Only accept these values: ${algorithmsFinal.join(", ")}`);
+	}
+	const info: CipherInfo | undefined = getCipherInfo(algorithm);
+	if (typeof info === "undefined") {
+		throw new Error(`Unable to get the information of the cipher algorithm \`${algorithm}\`!`);
+	}
+	return info;
+}
+function scryptPromise(...params: Parameters<typeof scryptSync>): Promise<ReturnType<typeof scryptSync>> {
+	return new Promise((resolve, reject): void => {
+		//@ts-ignore Overload.
+		scrypt(...params, (err, derivedKey): void => {
+			if ((err ?? null) === null) {
+				resolve(derivedKey);
+			} else {
+				reject(err);
+			}
+		});
+	});
+}
+export async function createSymmetricCrypto(key: BinaryLike, options: SymmetricCryptorOptions = {}): Promise<SymmetricCryptor> {
+	const {
+		algorithm = "aes-256-cbc",
+		scrypt: scryptOptions
+	}: SymmetricCryptorOptions = options;
+	const info: CipherInfo = resolveSymmetricCryptorInfo(algorithm);
+	const ivLength: number = info.ivLength ?? 16;
+	const keyScrypt: Uint8Array = Uint8Array.from(await scryptPromise(key, key, info.keyLength, scryptOptions));
+	const keyObject: KeyObject = createSecretKey(keyScrypt);
+	return new SymmetricCryptor(algorithm, ivLength, keyObject);
+}
+export function createSymmetricCryptoSync(key: BinaryLike, options: SymmetricCryptorOptions = {}): SymmetricCryptor {
+	const {
+		algorithm = "aes-256-cbc",
+		scrypt: scryptOptions
+	}: SymmetricCryptorOptions = options;
+	const info: CipherInfo = resolveSymmetricCryptorInfo(algorithm);
+	const ivLength: number = info.ivLength ?? 16;
+	const keyScrypt: Uint8Array = Uint8Array.from(scryptSync(key, key, info.keyLength, scryptOptions));
+	const keyObject: KeyObject = createSecretKey(keyScrypt);
+	return new SymmetricCryptor(algorithm, ivLength, keyObject);
+}
